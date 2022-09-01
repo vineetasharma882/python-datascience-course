acbb63c7879350e926c6b90b3fb4ddab7daa353b
vineetasharma882 <80589007+vineetasharma882@users.noreply.github.com>
2022-09-01 20:38:21 +0530

    datatypes and operator change

--- datatypes.py
+++ datatypes.py
@@ -1,41 +1,72 @@
+#int
 x=12348
 y=-34578399
 z= 85493837339202983847
 print(x, type(x))
 print(y, type(y))
 print(z, type(z))
+
+# float
+
 x=1.10
 y=2.0
 z=0.1234
 print(x, type(x))
 print(y, type(y))
 print(z, type(z))
+
+# string
+
 name="Vineeta Sharma"
 color= 'Blue'
 institute='digipodium'
 print(name, type(name))
 print(color, type(color))
 print(institute, type(institute))
+
+# Boolian
+
 code=False
 fun=True
 print(code, type(code))
 print(fun, type(fun))
+
+# None Type
+
 x=None
 print(x, type(x))
+
+# List
+
 a=[2,3,4,5,63]
 val=[2,1.3,'Python']
 color=['red','green']
 print(a, type(a))
 print(val, type(val))
 print(color, type(color))
+
+# Tupple
+
 a=(2,3,4,5,63)
 val=(2,1.3,'Python')
 color=('red','green')
 print(a, type(a))
 print(val, type(val))
 print(color, type(color))
-val=[2,1.3,'Python']
-color=['red','green']
+
+# Set
+
+a={2,3,4,5,63}
+val={2,1.3,'Python'}
+color={'red','green'}
 print(a, type(a))
 print(val, type(val))
 print(color, type(color))
+
+# Dictionary
+
+carinfo={
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964}
+print(carinfo)
--- op4.py
+++ op4.py
@@ -5,3 +5,10 @@ c=11
 print(a>b and a>c)
 print(a>b and a<c)
 print(a>b or a>c)
+print(a>b or a<c)
+print(a<b or a>c)
+print(a<b and a>c)
+
+print (not a>b)
+print(not False)
+print(not True)
